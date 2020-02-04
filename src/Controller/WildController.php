8119d0393f935d03c83ec4177ffd154f4eb21823
Flo <florence.sivadier@gmail.com>
2020-02-04 12:17:53 +0100

    add DefaultController for route and home template

---
+++ src/Controller/WildController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WildController extends AbstractController
+{
+    /**
+     * @route ("/wild", name="wild_index")
+     * @return Response
+     */
+    public function index() :Response
+    {
+        return $this->render('wild/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+}
