45ec4070891015ccb5f04189f844f02589b21dde
Flo <florence.sivadier@gmail.com>
2020-02-05 11:31:55 +0100

    Advanced routing

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -14,10 +14,26 @@ class WildController extends AbstractController
      * @route ("/wild", name="wild_index")
      * @return Response
      */
-    public function index() :Response
+//    public function index() :Response
+//    {
+//        return $this->render('wild/index.html.twig', [
+//            'website' => 'Wild Séries',
+//        ]);
+//    }
+    /**
+     * @Route("/wild/{slug}",
+     * requirements={"slug" = "[a-z0-9\-]+"},
+     * name="wild_show")
+     * @return Response
+     */
+    public function show(string $slug): Response
     {
-        return $this->render('wild/index.html.twig', [
-            'website' => 'Wild Séries',
-        ]);
+        $noSlug = "Aucune série séléctionnée, veuillez choisir une série";
+        if($slug=== '') {
+            $slug=$noSlug;
+        } else {
+            $slug=ucwords(str_replace('-', ' ',$slug));
+        }
+        return $this->render('wild/show.html.twig', ['slug' => $slug]);
     }
 }
