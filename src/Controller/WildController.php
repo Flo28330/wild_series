36f348c4039852c0baa93402fa0e9c30ec62b62d
Flo <florence.sivadier@gmail.com>
2020-02-06 10:24:24 +0100

    create database and entities

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -14,12 +14,12 @@ class WildController extends AbstractController
      * @route ("/wild", name="wild_index")
      * @return Response
      */
-//    public function index() :Response
-//    {
-//        return $this->render('wild/index.html.twig', [
-//            'website' => 'Wild Séries',
-//        ]);
-//    }
+    public function index() :Response
+    {
+        return $this->render('wild/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
     /**
      * @Route("/wild/{slug}",
      * requirements={"slug" = "[a-z0-9\-]+"},
